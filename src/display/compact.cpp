7d552d3d53ed1735020a98fc80b526a47bb0e725
asdfish <lgray3420@gmail.com>
2024-12-01 20:38:40 +0100

    added windows support

--- src/display/compact.cpp
+++ src/display/compact.cpp
@@ -1,14 +1,28 @@
 #include <display/compact.hpp>
 
+#ifdef __WIN32
+extern "C" {
+#include <windows.h>
+};
+#else
 extern "C" {
   #include <sys/ioctl.h>
   #include <unistd.h>
 };
+#endif // __WIN32
 
 std::string Compact::output(void) {
+#ifdef __WIN32
+  CONSOLE_SCREEN_BUFFER_INFO console_screen_buffer_info;
+  unsigned int width = 0;
+  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &console_screen_buffer_info);
+
+  width = console_screen_buffer_info.srWindow,Right - console_screen_buffer_info.srWindow.Left + 1;
+#else
   struct winsize window_size;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &window_size);
   unsigned int width = window_size.ws_col;
+#endif // __WIN32
 
   size_t length = 0;
 
